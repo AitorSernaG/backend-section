4ecd6e43dc202152d2c2f31c9dc197bffb92c02a
AitorSernaG <aitor.serna80@gmail.com>
2020-03-16 11:44:51 +0100

    [ADD] service added

---
+++ src/services/base.service.js
@@ -0,0 +1,67 @@
+class BaseService{
+
+    constructor(repository){
+        this.repository = repository;
+    }
+
+    async get(id){
+
+        if(!id){
+            const error = new Error();
+            error.status = 400;
+            error.message = "Id must be sent";
+            throw error; // el throw lo atrapa el mddleware que hicimos para los errores
+        }
+
+        const currentEntity = await this.repository.get(id);
+
+        if(!currentEntity){
+            const error = new Error();
+            error.status = 404;
+            error.message = "Entity does not found";
+            throw error;
+        }
+
+        return currentEntity;
+
+
+    }
+
+
+    async getAll(){
+        return await this.repository.getAll();
+    }
+
+
+    async create(entity){
+        return await this.repository.create(entity);
+    }
+
+
+    async update(id, entity){
+
+        if(!id){
+            const error = new Error();
+            error.status = 400;
+            error.message = "Id must be sent";
+            throw error; // el throw lo atrapa el mddleware que hicimos para los errores
+        }
+        return await this.repository.update(id, entity);
+
+    }
+
+    async delete(id){
+        if(!id){
+            const error = new Error();
+            error.status = 400;
+            error.message = "Id must be sent";
+            throw error; // el throw lo atrapa el mddleware que hicimos para los errores
+        }
+        return await this.repository.delete(id);
+    }
+
+}
+
+
+
+module.exports = BaseService;
--- src/startup/container.js
+++ src/startup/container.js
@@ -5,7 +5,8 @@ const config = require("../config");
 const app = require(".");
 
 // services
-const { HomeService } = require("../services");
+const { HomeService, UserService,  IdeaService, CommentService } = require("../services");
+
 
 // controllers
 const { HomeController } = require("../controllers");
@@ -31,7 +32,10 @@ container
     config: asValue(config)
 })
 .register({
-    HomeService: asClass(HomeService).singleton()
+    HomeService: asClass(HomeService).singleton(),
+    UserService: asClass(UserService).singleton(),
+    IdeaService: asClass(IdeaService).singleton(),
+    CommentService: asClass(CommentService).singleton()
 }).register({
     HomeController: asClass(HomeController.bind(HomeController)).singleton()
 }).register({
